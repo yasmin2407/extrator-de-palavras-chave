6ead1d30f7fed3e0bac82ebad4727008a4ed3078
Giulliana Cestari <dev.giullianacestari@gmail.com>
2024-10-10 17:22:05 -0300

    aula 6 - projeto 1 final

--- script.js
+++ script.js
@@ -12,7 +12,18 @@ function mostraPalavrasChave() {
 
 function processaTexto(texto) {
     let palavras = texto.split(/\P{L}+/u);
+    const frequencias = contaFrequencias(palavras);
+    let ordenadas = Object.keys(frequencias).sort(ordenaPalavra);
 
+    function ordenaPalavra(p1, p2) {
+        return frequencias[p2] - frequencias[p1];
+    }
+
+    console.log(ordenadas);
+    return ordenadas.slice(0, 10);
+}
+
+function contaFrequencias(palavras) {
     let frequencias = {};
     for (let i of palavras) {
         frequencias[i] = 0;
@@ -22,8 +33,5 @@ function processaTexto(texto) {
             }
         }
     }
-
-    console.log(frequencias);
-
-    return palavras;
+    return frequencias;
 }
