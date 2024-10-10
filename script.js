5497bfaaf0d21c653c5ef5fb61b9d75172189d1b
Giulliana Cestari <dev.giullianacestari@gmail.com>
2024-10-10 17:28:04 -0300

    aula 7 - lista de   palavras ruins

--- script.js
+++ script.js
@@ -12,6 +12,13 @@ function mostraPalavrasChave() {
 
 function processaTexto(texto) {
     let palavras = texto.split(/\P{L}+/u);
+
+    for (let i in palavras) {
+        palavras[i] = palavras[i].toLowerCase();
+    }
+
+    palavras = tiraPalavrasRuins(palavras);
+
     const frequencias = contaFrequencias(palavras);
     let ordenadas = Object.keys(frequencias).sort(ordenaPalavra);
 
@@ -34,4 +41,15 @@ function contaFrequencias(palavras) {
         }
     }
     return frequencias;
+}
+
+function tiraPalavrasRuins(palavras) {
+    const PALAVRAS_RUINS = new Set(["para", "uma", "nós"]);
+    const palavrasBoas = [];
+    for (let palavra of palavras) {
+        if (!PALAVRAS_RUINS.has(palavra) && palavra.length > 2) {
+            palavrasBoas.push(palavra);
+        }
+    }
+    return palavrasBoas;
 }
